993d39d131832de4b5060f92347d956309e87156
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-04-29 19:49:14 +0100

    Used Intent to send Data from one Activity to another

--- app/src/main/java/com/example/calebandroid/MainActivity.java
+++ app/src/main/java/com/example/calebandroid/MainActivity.java
@@ -20,30 +20,12 @@ public class MainActivity extends AppCompatActivity {
         setTitle("Home");
     }
 
-    public void onSubmit (View v) {
-        Button button = findViewById(v.getId());
-        EditText username = findViewById(R.id.inputUsername);
-        EditText email = findViewById(R.id.inputEmail);
-        EditText password = findViewById(R.id.inputPassword);
-
-        TextView usernameText = findViewById(R.id.textViewName);
-        TextView emailText = findViewById(R.id.textViewEmail);
-        TextView passwordText = findViewById(R.id.textViewPassword);
-
-        usernameText.setText(username.getText().toString());
-        emailText.setText(email.getText().toString());
-        passwordText.setText(password.getText().toString());
-
-        button.setText("SUBMITTED");
-        Toast.makeText(this,"Registered successfully", Toast.LENGTH_LONG).show();
-
-        Intent intent = new Intent(this, SettingsActivity.class);
-        intent.putExtra("username", username.getText().toString());
+    public void handleReply (View view) {
+        Button button = findViewById(view.getId());
+        Intent intent = new Intent(this, ComposeMessageActivity.class);
+        intent.putExtra("username", button.getText().toString().substring(5));
         startActivity(intent);
-
-        Log.d("myLog", "submit button clicked");
-        button.setEnabled(false);
-
-
     }
+
+
 }
